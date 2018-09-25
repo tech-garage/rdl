6ec37ea1c5f78c9300ac34845e20e6d95fdb0327
Vladimir Safin <vsafin@kdsconsulting.com>
2018-09-25 01:12:14 -0400

    Modified test

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,14 +1,13 @@
 import json
 import pytest
-from questions.application import application
+from questions.application import app
 
 @pytest.fixture
 def client():
-    return application.test_client()
+    return app.test_client()
 
 def test_response(client):
     result = client.get()
     response_body = json.loads(result.get_data())
     assert result.status_code == 200
-    assert result.headers['Content-Type'] == 'application/json'
-    assert response_body['Output'] == 'Hello World'
+
