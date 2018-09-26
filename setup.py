7c049bfdec630605a8cb96525ef647cba54880dd
Danny Dasilva <32214239+DannylDasilva@users.noreply.github.com>
2018-09-26 11:38:11 -0400

    added repositories

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'flask', 'flask_socketio'
+        'flask', 'flask_socketio', 'csv', 'random', 'json', 'uuid', 'jinja2'
     ],
 )
